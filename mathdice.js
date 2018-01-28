b7da11d3218f205eabcbedd5aa60626d9982801f
nupanick <nupanick@gmail.com>
2018-01-28 01:36:00 -0800

    Cleaned up the page significantly. No more pre-emptive generalizations.

--- mathdice.js
+++ mathdice.js
@@ -1,34 +1,24 @@
-"use strict"
-const numDice = 3;
+"use strict";
 
-// Returns a lambda for ease of re-rolling.
-const roll_d = (x) =>
-        () => (x * Math.random() + 1)|0
+// Assign event handlers.
+document.getElementById("roll-button").onclick = rollDice;
 
-// Applies a function repeatedly. Only useful for impure functions.
-        const do_times = (n, f) => {
-        return map(Array(n), f)
+/**
+ * Generate new values for all the dice on the page.
+ */
+function rollDice () {
+	// mouse:mice :: douse:dice
+	const diceList = document.querySelectorAll('#dicebox > .dice');
+	Array.prototype.forEach.call(diceList, douse => {
+		douse.innerHTML = roll_d(n);
+	})
 }
 
-// Sets the display values of the dice.
-function setDiceOutput (nums) {
-        const dicebox = document.querySelectorAll('#dicebox > .dice')
-        Array.prototype.map.call(dicebox, (die, index) =>
-                die.innerHTML = nums[index]
-        )
+/**
+ * Generate a random number between 1 and n, inclusive.
+ * @param n
+ */
+function roll_d (n) {
+	const roll = (n * Math.random() |0) + 1;
+	return roll;
 }
-
-// Roll all the dice on the page!
-function rollAllDice() {
-        const dicebox = document.querySelectorAll('#dicebox > .dice')
-        Array.prototype.map.call(dicebox, die =>
-                die.innerHTML = roll_d(6)()
-        )
-}
-
-const rollButton = document.getElementById('roll-button')
-rollButton.onclick = rollAllDice;
-
-//console.log(do_times(3, roll_d(6)))
-setDiceOutput([6, 6, 6])
-
